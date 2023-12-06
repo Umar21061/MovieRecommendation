4adf1880393370e683a55943c6d7ef240bba346a
Umar Murtaza <138823435+Umar21061@users.noreply.github.com>
2023-12-06 00:15:03 -0800

    update

--- MovieRecommendation/settings.py
+++ MovieRecommendation/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+# ...
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
